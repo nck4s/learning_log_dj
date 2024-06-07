babc8baebcb0481166b28869477cb2adfa72fc71
nck4s <nn.2nn@mail.ru>
2024-06-07 16:08:53 +1000

    templates

--- l_l/views.py
+++ l_l/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'topics.html', context)
