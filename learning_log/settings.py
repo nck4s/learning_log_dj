45d5f3682cd65c61b09e6ad3dda80637e7f338ab
nck4s <nn.2nn@mail.ru>
2024-06-12 16:42:00 +1000

    commit

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'l_l'
+    'l_l',
     'users',
 ]
 
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/users/login/'
